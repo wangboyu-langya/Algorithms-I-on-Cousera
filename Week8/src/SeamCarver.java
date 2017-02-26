50ffd8a79b187ce6b29a97cfb9701a17b19232f2
xianglong hu <xianglong hu>
2017-02-26 15:21:05 +0800

    finished yet a little slow

--- Week8/src/SeamCarver.java
+++ Week8/src/SeamCarver.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Picture;
 
 import java.awt.*;
@@ -11,10 +12,22 @@ import java.awt.*;
  * 2.use greedy algorithm to find the shortest path. use a priority queue to store all the information. And start
  * from the shortest one.
  * <p>
+ * <p>
  * Problems at the moment:
  * 1. Although the requirement is that the performance requirement is at most linear to all the pixels in the
  * figure. I want to find an efficient way to implement it. After discussion with Kang, I convinced myself that
  * there is no better way.
+ * 2. The algorithm used to find the shortest path is the same as the Dijkstra's algorithm or the topological sort.
+ * However, I don't quite understand the mechanism of topological sort.
+ * I only have a general idea how the topological order works, but since there is no explicit digraph here, I
+ * think I would rather not use the topological sort algorithm. Instead, I should improve the Dijkstra's algorithm.
+ * The only difference is that the edge is no longer important, I should only using the nodes, and the other things
+ * are approximately the same.
+ * To be more specific, setting an array of distance, setting an array of node, always deque the node with the
+ * shortest gradient, and add its neighbors, i.e the next three pixels in the next row.
+ * When adding to the queue, the edge and distance should be changed.
+ * 3. If using the algorithm above, the difficulty should be the transformation between the row, col number and a single
+ * number.
  */
 public class SeamCarver {
     private Picture current;
@@ -52,27 +65,39 @@ public class SeamCarver {
         squareDeltaX = rX * rX + gX * gX + bX * bX;
         squareDeltaY = rY * rY + gY * gY + bY * bY;
         gradient = Math.sqrt(squareDeltaX + squareDeltaY);
-        return  gradient;
+        return gradient;
     }
 
     public int width()                            // width of current picture
-    {
-        return width;
-    }
+    {  return width;  }
 
     public int height()                           // height of current picture
-    {
-        return  height;
-    }
+    {  return height;  }
 
     public double energy(int x, int y)               // energy of pixel at column x and row y
-    {
-        return gradients[x][y];
-    }
+    {  return gradients[x][y];  }
 
     public int[] findHorizontalSeam()               // sequence of indices for horizontal seam
     {
-         
+          MinPQ<Double> pq = new MinPQ();
+          int[] nodeTo = new int[width * height];  // record the last node to it
+          double[] distanceTo = new double[width * height];  // record the distance to the node
+        for (int v = 0; v < ; v++) {
+            distanceTo[v] = Double.POSITIVE_INFINITY;
+        }
+        distanceTo[0] = 0.0;
+        pq.insert(s, 0.0);
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            for (int n : adj(v)) {
+                relax(n);
+            }
+        }
+
+
+    }
+
+    private int[] adj(int v) {
     }
 
     public int[] findVerticalSeam()                 // sequence of indices for vertical seam
